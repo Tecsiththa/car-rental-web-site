86d0a09b37d021defeff27ad0ca9671d31c3a2b3
Sithum Wijethunga <128587286+Tecsiththa@users.noreply.github.com>
2025-08-02 20:46:47 +0530

    add dark mode feature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BiSolidMoon, BiSolidSun } from 'react-icons/bi'
 
 const NavLinks = [
     {
@@ -22,39 +23,52 @@ const NavLinks = [
         link:"/#booking",
     },
     ]
-export const Navbar = () => {
-  return (
-   <nav className="shadow-md">
-    <div className="container">
 
+
+export const Navbar = ({ theme, setTheme }) => {
+  return (
+    <nav className="shadow-md bg-white dark:bg-dark dark:text-white duration-300">
+      <div className="container">
         <div className="flex justify-between items-center">
 
-            <div>
-                <h1 className="text-3xl font-bold font-serif">Car Rental</h1>
-            </div>
+          {/* Logo */}
+          <div>
+            <h1 className="text-3xl font-bold font-serif">Car Rental</h1>
+          </div>
 
-            <div className="hidden md:block">
-                <ul className="flex items-center gap-6">
-                    {
-                        NavLinks.map((data) =>(
+          {/* Navigation Links */}
+          <div className="hidden md:block">
+            <ul className="flex items-center gap-6">
+              {NavLinks.map((data) => (
+                <li key={data.id} className="py-4">
+                  <a
+                    href={data.link}
+                    className="py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium"
+                  >
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                            <li key={data.link} className="py-4">
-                        <a className=" py-2 hover:border-b2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium" href={data.link}>
-                            {data.name}
-    
-                        </a>
-                    </li>
-
-                        ))
-                    }
-                    
-                </ul>
-            </div>
+          {/* Theme Toggle */}
+          <div>
+            {theme === "dark" ? (
+              <BiSolidSun
+                onClick={() => setTheme("light")}
+                className="text-2xl cursor-pointer"
+              />
+            ) : (
+              <BiSolidMoon
+                onClick={() => setTheme("dark")}
+                className="text-2xl cursor-pointer"
+              />
+            )}
+          </div>
 
         </div>
-
-    </div>
-
-   </nav>
-  )
-}
+      </div>
+    </nav>
+  );
+};
